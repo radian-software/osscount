b7d9735330a1e1ae6f0f54466580da4bd71c83d2
Radon Rosborough <radon.neon@gmail.com>
2021-08-22 22:19:57 -0700

    Add CLI skeleton

---
+++ osscount.py
@@ -0,0 +1,12 @@
+import argparse
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser("osscount")
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
